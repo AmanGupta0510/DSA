fcbf17fdd111df70c4931cc1209f99b2e1b85bf1
AmanGupta0510 <amanrajgupta7677@gmail.com>
2025-11-16 00:28:19 +0530

    solution

---
+++ SubsequencesPattern/GenerateBinaryString.java
@@ -0,0 +1,27 @@
+// Question : Generate all binary strings of N bits
+// Problem link : https://www.geeksforgeeks.org/problems/generate-all-binary-strings/1
+// Platform : GeeksforGeeks
+
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<String> binstr(int n) {
+        // code here
+        ArrayList<String> res = new ArrayList<>();
+        String bits = "";
+        helper(n,res,bits);
+        return res;
+        
+    }
+    public void helper(int n,ArrayList<String> res,String bits){
+        
+        if(n==bits.length()){ // baseCase - when length of bits is equal to n then add it to the res.  
+            res.add(bits);
+            return;
+        }
+        // add '0'in bits and call helper function recursively
+        helper(n,res,bits+'0');
+        // add '1'in bits and call helper function recursively.
+        helper(n,res,bits+'1');
+    }
+}
