78527b90104c51f4ec728f22e13032653d6caee6
AmanGupta0510 <amanrajgupta7677@gmail.com>
2025-12-01 15:07:51 +0530

    Solution  with Explanation

---
+++ LeetCode_Contest/Weekly_contest_478/CountElementsWithAtLeastKGreaterValues.java
@@ -0,0 +1,30 @@
+package LeetCode_Contest.Weekly_contest_478;
+import java.util.*;
+class Solution {
+    public int countElements(int[] nums, int k) {
+
+        Arrays.sort(nums);//sort the input arrray
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(possible(nums,k,nums[i]))count+=1; // for each number do a binary search to get the count of numbers greater than the current num in logarithmic time.
+        }
+        return count;
+    }
+
+    public boolean possible(int[] nums ,int k,int n){
+        int low = 0;
+        int high  = nums.length-1;
+        while(low<=high){
+            int mid = (low+high) / 2;
+            if(nums[mid]<=n)low=mid+1;
+            else high = mid-1;
+        }
+        return nums.length - low >= k; // here low is the first index which is greater than n.and we assume that all the elements after low is also strictly greater than n bcz array is sorted. 
+    } 
+}
+
+/*
+Explanation :-
+Approach 1 - (Sorting And Binary Search)
+
+*/
